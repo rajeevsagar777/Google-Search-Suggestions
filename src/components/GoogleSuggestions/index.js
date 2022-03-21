ba6b711423b8260deea76a3df539c952d0bbfb27
rajeevsagar <rajeev22335@gmail.com>
2022-03-21 16:00:22 +0000

    adding arrow in button element

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,57 +1,68 @@
 import {Component} from 'react'
 
-import './index.css'
 import SuggestionItem from '../SuggestionItem'
 
-class GoogleSuggestions extends Component {
-  state = {searchInput: ''}
+import './index.css'
 
-  onChangeSearchInput = event => {
-    console.log(event.target.value)
-    this.setState({searchInput: event.target.value})
+class GoogleSuggestions extends Component {
+  state = {
+    searchInput: '',
   }
 
-  showClickedItem = value => {
-    this.setState({searchInput: value})
+  updateSearchInput = value => {
+    this.setState({
+      searchInput: value,
+    })
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
   }
 
   render() {
     const {searchInput} = this.state
     const {suggestionsList} = this.props
-    const isSearchedValue = suggestionsList.filter(eachItem =>
-      eachItem.suggestion.toLowerCase().includes(searchInput.toLowerCase()),
+    const searchResults = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
     )
+
     return (
       <div className="app-container">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
-          alt="google logo"
-          className="google-logo"
-        />
-        <div className="google-card-container">
-          <div className="search-container">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
-              alt="search icon"
-              className="search-icon"
-            />
-            <input
-              type="search"
-              className="input-el"
-              placeholder="Search Google"
-              value={searchInput}
-              onChange={this.onChangeSearchInput}
-            />
-          </div>
-          <ul className="list-container">
-            {isSearchedValue.map(eachItem => (
-              <SuggestionItem
-                suggestionEl={eachItem}
-                key={eachItem.id}
-                showClickedItem={this.showClickedItem}
+        <div className="google-suggestions-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google logo"
+            className="google-logo"
+          />
+          <div className="search-input-suggestions-container">
+            <div className="search-input-container">
+              <img
+                alt="search icon"
+                className="search-icon"
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
               />
-            ))}
-          </ul>
+              <input
+                type="search"
+                className="search-input"
+                placeholder="Search Google"
+                onChange={this.onChangeSearchInput}
+                value={searchInput}
+              />
+            </div>
+            <ul className="suggestions-list">
+              {searchResults.map(eachSuggestion => (
+                <SuggestionItem
+                  key={eachSuggestion.id}
+                  suggestionDetails={eachSuggestion}
+                  updateSearchInput={this.updateSearchInput}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     )
--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,19 +1,27 @@
 import './index.css'
 
 const SuggestionItem = props => {
-  const {suggestionEl, showClickedItem} = props
-  const {suggestion} = suggestionEl
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
 
-  const clickedSuggestion = event => showClickedItem(event.target.value)
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+
   return (
-    <li className="list-item">
-      <p className="content">{suggestion}</p>
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
-        className="arrow"
-        alt="arrow"
-        onClick={clickedSuggestion}
-      />
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
+        onClick={onClickSuggestion}
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
     </li>
   )
 }
